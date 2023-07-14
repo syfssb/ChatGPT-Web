2f5479d15498d6c4bc5a4d776ca4962ee33368a0
syfssb <1073634403@qq.com>
2023-07-14 20:50:30 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://i1.hdslb.com/bfs/face/7b4d541ec689a1fa1bd40399360864dd4b53d307.jpg',
+      name: 'Jsnono',
+      description: '内部使用，禁止分享！',
     },
   }
 }
